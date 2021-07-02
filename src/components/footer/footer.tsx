0cd7f68e8db73cd7fa2ff2e12ae59b6df369db8f
eviebutland <evie.butland@gmail.com>
2021-07-02 21:58:17 +0100

    building out footer styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,13 @@
+import styles from './footer.module.scss'
+import linkedInIcon from '../../assets/svg/linkedin-in-brands.svg'
+
 export const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <a href="mailto:evie.butland@gmail.com">evie.butland@gmail.com</a>
       <a href="tel:07720764111">07720764111</a>
-      <a href="https://www.linkedin.com/in/evie-butland-432a33170">
-        <img/>
+      <a className={styles.icon} href="https://www.linkedin.com/in/evie-butland-432a33170">
+        <img src={linkedInIcon} alt="linked in icon"/>
       </a>
     </footer>
   )
--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,7 +1,9 @@
+import styles from './timeline.module.scss'
+
 export const Timeline = () => {
   return (
-    <div>
-      <div>2018 {new Date().getFullYear()}</div>
+    <div className={styles.container}>
+      <div className={styles.date_container}>2018 {new Date().getFullYear()}</div>
       <div>
         <p>Bit of text here about me but not a lot. Bit of text here about me but not a lot Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot</p>
         <p>Bit of text here about me but not a lot. Bit of text here about me but not a lot Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot. Bit of text here about me but not a lot</p>
