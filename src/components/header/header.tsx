8c98e8b5adf028ae398543ac43fa7f7498990ed1
EvieButland <evie.butland@hectare.farm>
2023-07-12 08:13:30 +0100

    header changes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,19 @@
-import { Button } from "../button/button";
-
-import bitemoji from "../../assets/images/image-of-me.png";
-
-import { useState } from "react";
 import me from "../../assets/svg/me.svg";
 
 export const Header = () => {
-  const [image, setImage] = useState(bitemoji);
+  const yearsExperience = new Date().getFullYear() - 2019;
+
   return (
-    <section className="flex my-20 justify-center items-center">
-      <div>
-        <h1 className="text-large">Hi, I'm Evie</h1>
+    <section className="flex flex-col-reverse md:flex-row m-10 md:m-20 md:justify-center items-center">
+      <div className="mt-10 md:mt-0 text-center md:text-left">
+        <h1 className=" md:mb-2 lg:mb-10">Hi, I'm Evie</h1>
         <h1 className="max-w-[700px]">Front-End Software Engineer</h1>
+        <p className="mt-10">
+          Over {yearsExperience} years experience working with Front-end
+          technologies
+        </p>
       </div>
-      <img src={me} alt="bitemoji of me" />
+      <img src={me} alt="me" className="w-[150px] md:w-[300px] lg:w-auto" />
     </section>
   );
 };
